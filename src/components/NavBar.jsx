6b87fde54534dfcc563e4b08fde3251328db618a
Fausto Leal <fausto.leal2099@gmail.com>
2025-08-06 11:30:26 -0300

    modificando los estilos del NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,19 @@
 const NavBar = () => {
   return (
     <nav className="navBar">
-      <button>
-        <img src="./images/icon-menu.svg" alt="menu" />
-      </button>
-      <img src="./images/logo.svg" alt="sneakers" />
+      <img className="menu-icon" src="./images/icon-menu.svg" alt="menu" />
+      <img className="logo" src="./images/logo.svg" alt="sneakers" />
+      <section className="links">
+        <a>Collections</a>
+        <a>Men</a>
+        <a>Women</a>
+        <a>About</a>
+        <a>Contact</a>
+      </section>
+      <section className="usuario">
+        <img className="cart" src="./images/icon-cart.svg" alt="cart" />
+        <img className="avatar" src="./images/image-avatar.png" alt="avatar" />
+      </section>
     </nav>
   );
 };
